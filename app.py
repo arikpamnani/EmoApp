af30ac147023f1112c06a188c2327761cebdd72c
arikpamnani <arikpamnani@gmail.com>
2018-11-06 18:25:30 +0530

    +++

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import os
-os.environ["KERAS_BACKEND"] = "theano"
-
 import json, pickle, re, time 
 from flask import Flask, render_template, request, make_response, jsonify
 from keras.models import load_model
